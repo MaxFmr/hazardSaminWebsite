1127b0068eb9142fef35bc6352af16887f26e817
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2022-10-18 11:27:32 +0200

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <GoogleAnalytics trackPageViews />
+      {/* google */}
 
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
